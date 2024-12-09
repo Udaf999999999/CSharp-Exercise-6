3d75485cf42297da2a84db86765661f2cd40187d
Udaf999999999 <udaf999999999@yandex.ru>
2024-12-09 15:15:41 +0700

    Add class user

--- OOPConsoleApp/Program.cs
+++ OOPConsoleApp/Program.cs
@@ -69,7 +69,80 @@ namespace OOPConsoleApp
     {
 
     }
+    public class TrafficLight
+    {
+        private void ChangeColor(string color)
+        {
 
+        }
+        public string GetColor()
+        {
+            return null;
+        }
+    }
+    class User
+    {
+        private int age;
+        private string login;
+        private string mail;
+
+        public int Age
+        {
+            get
+            {
+                return age;
+            }
+
+            set
+            {
+                if (value < 18)
+                {
+                    Console.WriteLine("Возраст должен быть не меньше 18");
+                }
+                else
+                {
+                    age = value;
+                }
+            }
+        }
+        public string Login
+        {
+            get
+            {
+                return login;
+            }
+            set
+            {
+                if (value.Length < 3)
+                {
+                    Console.WriteLine("Name's length shorter 3!");
+                }
+                else
+                {
+                    login = value;
+                }
+            }
+        }
+        public string Mail
+        {
+            get
+            {
+                return mail;
+            }
+            set
+            {
+                if (value.Contains('@'))
+                {
+                    mail = value;
+                }
+                else
+                {
+                    Console.WriteLine("Email must contain @");
+                }
+
+            }
+        }
+    }
     internal class Program
     {
         static void Main(string[] args)
